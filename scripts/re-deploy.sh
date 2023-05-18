871ee6017ddd98408f21efd5454facf4646fa439
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-05-18 19:02:08 +0200

    extra sudos to make script work lolz

--- scripts/re-deploy.sh
+++ scripts/re-deploy.sh
@@ -3,6 +3,6 @@
 vm_name="infra-assignment-vm-fifi"
 zone="europe-west1-b"
 
-gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl stop infra-app.service" --quiet
-gcloud compute ssh "$vm_name" --zone="$zone" --command="cd /infra-app && git reset --hard && git pull && gcloud storage cp --recursive gs://infra-bucket-230516/static /infra-app && npm i && npm run build" --quiet
-gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl start infra-app.service" --quiet
+gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl stop infra-app.service"
+gcloud compute ssh "$vm_name" --zone="$zone" --command="cd /infra-app && sudo git reset --hard && sudo git pull && sudo gcloud storage cp --recursive gs://infra-bucket-230516/static /infra-app && sudo npm i && sudo npm run build"
+gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl start infra-app.service"
