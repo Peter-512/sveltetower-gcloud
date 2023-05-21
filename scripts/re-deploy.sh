24ec12345d1df5041ceeff65560aefe625deca46
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-05-21 18:39:40 +0200

    not installing dev dependencies on server

--- scripts/re-deploy.sh
+++ scripts/re-deploy.sh
@@ -4,5 +4,5 @@ vm_name="infra-assignment-vm-fifi"
 zone="europe-west1-b"
 
 gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl stop infra-app.service"
-gcloud compute ssh "$vm_name" --zone="$zone" --command="cd /infra-app && sudo git reset --hard && sudo git pull && sudo npm i && sudo npm run build"
+gcloud compute ssh "$vm_name" --zone="$zone" --command="cd /infra-app && sudo git reset --hard && sudo git pull && sudo npm ci --omit dev && sudo npm run build"
 gcloud compute ssh "$vm_name" --zone="$zone" --command="sudo systemctl start infra-app.service"
