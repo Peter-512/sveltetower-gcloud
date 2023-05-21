36cbd5bac0807464933b036a663301f2fb4ec651
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-05-21 19:24:54 +0200

    https hopefully?

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
 import mkcert from "vite-plugin-mkcert"
+import fs from "fs"
 
 export default defineConfig({
 	server: {
 		https: true,
+	},
+	https: {
+		key: fs.readFileSync(
+			"/etc/letsencrypt/live/sveltetower.tech/privkey.pem"
+		),
+		cert: fs.readFileSync(
+			"/etc/letsencrypt/live/sveltetower.tech/fullchain.pem"
+		),
 	},
 	plugins: [sveltekit(), mkcert()],
 })
